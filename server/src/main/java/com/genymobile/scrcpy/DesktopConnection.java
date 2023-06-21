Genymobile/scrcpy

commit 8c650e53cd37a53d5c3aa746c30a71c6b742a4e2

Add --no-video
Similar to --no-audio, add --no-video to play audio only.

Fixes #3842 <https://github.com/Genymobile/scrcpy/issues/3842>
PR #3978 <https://github.com/Genymobile/scrcpy/pull/3978>

server/src/main/java/com/genymobile/scrcpy/DesktopConnection.java
@@ -41,7 +41,7 @@ public final class DesktopConnection implements Closeable {
             controlInputStream = null;
             controlOutputStream = null;
         }
-        videoFd = videoSocket.getFileDescriptor();
+        videoFd = videoSocket != null ? videoSocket.getFileDescriptor() : null;
         audioFd = audioSocket != null ? audioSocket.getFileDescriptor() : null;
     }
 
@@ -60,8 +60,11 @@ public final class DesktopConnection implements Closeable {
         return SOCKET_NAME_PREFIX + String.format("_%08x", scid);
     }
 
-    public static DesktopConnection open(int scid, boolean tunnelForward, boolean audio, boolean control, boolean sendDummyByte) throws IOException {
+    public static DesktopConnection open(int scid, boolean tunnelForward, boolean video, boolean audio, boolean control, boolean sendDummyByte)
+            throws IOException {
         String socketName = getSocketName(scid);
+
+        LocalSocket firstSocket = null;
 
         LocalSocket videoSocket = null;
         LocalSocket audioSocket = null;
@@ -69,20 +72,31 @@ public final class DesktopConnection implements Closeable {
         try {
             if (tunnelForward) {
                 try (LocalServerSocket localServerSocket = new LocalServerSocket(socketName)) {
-                    videoSocket = localServerSocket.accept();
-                    if (sendDummyByte) {
-                        // send one byte so the client may read() to detect a connection error
-                        videoSocket.getOutputStream().write(0);
+                    if (video) {
+                        videoSocket = localServerSocket.accept();
+                        firstSocket = videoSocket;
                     }
                     if (audio) {
                         audioSocket = localServerSocket.accept();
+                        if (firstSocket == null) {
+                            firstSocket = audioSocket;
+                        }
                     }
                     if (control) {
                         controlSocket = localServerSocket.accept();
+                        if (firstSocket == null) {
+                            firstSocket = controlSocket;
+                        }
+                    }
+                    if (sendDummyByte) {
+                        // send one byte so the client may read() to detect a connection error
+                        firstSocket.getOutputStream().write(0);
                     }
                 }
             } else {
-                videoSocket = connect(socketName);
+                if (video) {
+                    videoSocket = connect(socketName);
+                }
                 if (audio) {
                     audioSocket = connect(socketName);
                 }
@@ -106,10 +120,22 @@ public final class DesktopConnection implements Closeable {
         return new DesktopConnection(videoSocket, audioSocket, controlSocket);
     }
 
+    private LocalSocket getFirstSocket() {
+        if (videoSocket != null) {
+            return videoSocket;
+        }
+        if (audioSocket != null) {
+            return audioSocket;
+        }
+        return controlSocket;
+    }
+
     public void close() throws IOException {
-        videoSocket.shutdownInput();
-        videoSocket.shutdownOutput();
-        videoSocket.close();
+        if (videoSocket != null) {
+            videoSocket.shutdownInput();
+            videoSocket.shutdownOutput();
+            videoSocket.close();
+        }
         if (audioSocket != null) {
             audioSocket.shutdownInput();
             audioSocket.shutdownOutput();
@@ -130,7 +156,8 @@ public final class DesktopConnection implements Closeable {
         System.arraycopy(deviceNameBytes, 0, buffer, 0, len);
         // byte[] are always 0-initialized in java, no need to set '\0' explicitly
 
-        IO.writeFully(videoFd, buffer, 0, buffer.length);
+        FileDescriptor fd = getFirstSocket().getFileDescriptor();
+        IO.writeFully(fd, buffer, 0, buffer.length);
     }
 
     public FileDescriptor getVideoFd() {

app/data/bash-completion/scrcpy
@@ -37,6 +37,7 @@ _scrcpy() {
         --no-key-repeat
         --no-mipmaps
         --no-power-on
+        --no-video
         --otg
         -p --port=
         --power-off-on-close

app/data/zsh-completion/_scrcpy
@@ -43,6 +43,7 @@ arguments=(
     '--no-key-repeat[Do not forward repeated key events when a key is held down]'
     '--no-mipmaps[Disable the generation of mipmaps]'
     '--no-power-on[Do not power on the device on start]'
+    '--no-video[Disable video forwarding]'
     '--otg[Run in OTG mode \(simulating physical keyboard and mouse\)]'
     {-p,--port=}'[\[port\[\:port\]\] Set the TCP port \(range\) used by the client to listen]'
     '--power-off-on-close[Turn the device screen off when closing scrcpy]'

app/scrcpy.1
@@ -226,6 +226,10 @@ If the renderer is OpenGL 3.0+ or OpenGL ES 2.0+, then mipmaps are automatically
 Do not power on the device on start.
 
 .TP
+.B \-\-no\-video
+Disable video forwarding.
+
+.TP
 .B \-\-otg
 Run in OTG mode: simulate physical keyboard and mouse, as if the computer keyboard and mouse were plugged directly to the device via an OTG cable.
 

app/src/cli.c
@@ -73,6 +73,7 @@ enum {
     OPT_AUDIO_BUFFER,
     OPT_AUDIO_OUTPUT_BUFFER,
     OPT_NO_DISPLAY,
+    OPT_NO_VIDEO,
 };
 
 struct sc_option {
@@ -406,6 +407,11 @@ static const struct sc_option options[] = {
         .longopt_id = OPT_NO_POWER_ON,
         .longopt = "no-power-on",
         .text = "Do not power on the device on start.",
+    },
+    {
+        .longopt_id = OPT_NO_VIDEO,
+        .longopt = "no-video",
+        .text = "Disable video forwarding.",
     },
     {
         .longopt_id = OPT_OTG,
@@ -1797,6 +1803,9 @@ parse_args_with_getopt(struct scrcpy_cli_args *args, int argc, char *argv[],
             case OPT_NO_DOWNSIZE_ON_ERROR:
                 opts->downsize_on_error = false;
                 break;
+            case OPT_NO_VIDEO:
+                opts->video = false;
+                break;
             case OPT_NO_AUDIO:
                 opts->audio = false;
                 break;
@@ -2042,12 +2051,18 @@ parse_args_with_getopt(struct scrcpy_cli_args *args, int argc, char *argv[],
 #endif
 
 #ifdef HAVE_USB
-    if (!opts->mirror && opts->control && !opts->otg) {
+    if (!(opts->mirror && opts->video) && !opts->otg) {
 #else
-    if (!opts->mirror && opts->control) {
+    if (!(opts->mirror && opts->video)) {
 #endif
-        LOGD("Mirroring is disabled, force --no-control");
+        // If video mirroring is disabled and OTG are disabled, then there is
+        // no way to control the device.
         opts->control = false;
+    }
+
+    if (!opts->video) {
+        // If video is disabled, then scrcpy must exit on audio failure.
+        opts->require_audio = true;
     }
 
     return true;

app/src/options.c
@@ -73,6 +73,7 @@ const struct scrcpy_options scrcpy_options_default = {
     .cleanup = true,
     .start_fps_counter = false,
     .power_on = true,
+    .video = true,
     .audio = true,
     .require_audio = false,
     .list_encoders = false,

app/src/options.h
@@ -156,6 +156,7 @@ struct scrcpy_options {
     bool cleanup;
     bool start_fps_counter;
     bool power_on;
+    bool video;
     bool audio;
     bool require_audio;
     bool list_encoders;

app/src/recorder.c
@@ -152,7 +152,7 @@ sc_recorder_close_output_file(struct sc_recorder *recorder) {
 
 static inline bool
 sc_recorder_has_empty_queues(struct sc_recorder *recorder) {
-    if (sc_vecdeque_is_empty(&recorder->video_queue)) {
+    if (recorder->video && sc_vecdeque_is_empty(&recorder->video_queue)) {
         // The video queue is empty
         return true;
     }
@@ -176,7 +176,7 @@ sc_recorder_process_header(struct sc_recorder *recorder) {
         sc_cond_wait(&recorder->stream_cond, &recorder->mutex);
     }
 
-    if (sc_vecdeque_is_empty(&recorder->video_queue)) {
+    if (recorder->video && sc_vecdeque_is_empty(&recorder->video_queue)) {
         assert(recorder->stopped);
         // If the recorder is stopped, don't process anything if there are not
         // at least video packets
@@ -184,7 +184,11 @@ sc_recorder_process_header(struct sc_recorder *recorder) {
         return false;
     }
 
-    AVPacket *video_pkt = sc_vecdeque_pop(&recorder->video_queue);
+    AVPacket *video_pkt = NULL;
+    if (!sc_vecdeque_is_empty(&recorder->video_queue)) {
+        assert(recorder->video);
+        video_pkt = sc_vecdeque_pop(&recorder->video_queue);
+    }
 
     AVPacket *audio_pkt = NULL;
     if (!sc_vecdeque_is_empty(&recorder->audio_queue)) {
@@ -196,17 +200,19 @@ sc_recorder_process_header(struct sc_recorder *recorder) {
 
     int ret = false;
 
-    if (video_pkt->pts != AV_NOPTS_VALUE) {
-        LOGE("The first video packet is not a config packet");
-        goto end;
-    }
+    if (video_pkt) {
+        if (video_pkt->pts != AV_NOPTS_VALUE) {
+            LOGE("The first video packet is not a config packet");
+            goto end;
+        }
 
-    assert(recorder->video_stream_index >= 0);
-    AVStream *video_stream =
-        recorder->ctx->streams[recorder->video_stream_index];
-    bool ok = sc_recorder_set_extradata(video_stream, video_pkt);
-    if (!ok) {
-        goto end;
+        assert(recorder->video_stream_index >= 0);
+        AVStream *video_stream =
+            recorder->ctx->streams[recorder->video_stream_index];
+        bool ok = sc_recorder_set_extradata(video_stream, video_pkt);
+        if (!ok) {
+            goto end;
+        }
     }
 
     if (audio_pkt) {
@@ -218,13 +224,13 @@ sc_recorder_process_header(struct sc_recorder *recorder) {
         assert(recorder->audio_stream_index >= 0);
         AVStream *audio_stream =
             recorder->ctx->streams[recorder->audio_stream_index];
-        ok = sc_recorder_set_extradata(audio_stream, audio_pkt);
+        bool ok = sc_recorder_set_extradata(audio_stream, audio_pkt);
         if (!ok) {
             goto end;
         }
     }
 
-    ok = avformat_write_header(recorder->ctx, NULL) >= 0;
+    bool ok = avformat_write_header(recorder->ctx, NULL) >= 0;
     if (!ok) {
         LOGE("Failed to write header to %s", recorder->filename);
         goto end;
@@ -233,7 +239,9 @@ sc_recorder_process_header(struct sc_recorder *recorder) {
     ret = true;
 
 end:
-    av_packet_free(&video_pkt);
+    if (video_pkt) {
+        av_packet_free(&video_pkt);
+    }
     if (audio_pkt) {
         av_packet_free(&audio_pkt);
     }
@@ -263,7 +271,8 @@ sc_recorder_process_packets(struct sc_recorder *recorder) {
         sc_mutex_lock(&recorder->mutex);
 
         while (!recorder->stopped) {
-            if (!video_pkt && !sc_vecdeque_is_empty(&recorder->video_queue)) {
+            if (recorder->video && !video_pkt &&
+                    !sc_vecdeque_is_empty(&recorder->video_queue)) {
                 // A new packet may be assigned to video_pkt and be processed
                 break;
             }
@@ -277,6 +286,11 @@ sc_recorder_process_packets(struct sc_recorder *recorder) {
 
         // If stopped is set, continue to process the remaining events (to
         // finish the recording) before actually stopping.
+
+        // If there is no video, then the video_queue will remain empty forever
+        // and video_pkt will always be NULL.
+        assert(recorder->video || (!video_pkt
+                && sc_vecdeque_is_empty(&recorder->video_queue)));
 
         // If there is no audio, then the audio_queue will remain empty forever
         // and audio_pkt will always be NULL.
@@ -319,6 +333,9 @@ sc_recorder_process_packets(struct sc_recorder *recorder) {
             if (!recorder->audio) {
                 assert(video_pkt);
                 pts_origin = video_pkt->pts;
+            } else if (!recorder->video) {
+                assert(audio_pkt);
+                pts_origin = audio_pkt->pts;
             } else if (video_pkt && audio_pkt) {
                 pts_origin = MIN(video_pkt->pts, audio_pkt->pts);
             } else if (recorder->stopped) {
@@ -639,7 +656,7 @@ sc_recorder_audio_packet_sink_disable(struct sc_packet_sink *sink) {
 
 bool
 sc_recorder_init(struct sc_recorder *recorder, const char *filename,
-                 enum sc_record_format format, bool audio,
+                 enum sc_record_format format, bool video, bool audio,
                  const struct sc_recorder_callbacks *cbs, void *cbs_userdata) {
     recorder->filename = strdup(filename);
     if (!recorder->filename) {
@@ -662,6 +679,8 @@ sc_recorder_init(struct sc_recorder *recorder, const char *filename,
         goto error_queue_cond_destroy;
     }
 
+    assert(video || audio);
+    recorder->video = video;
     recorder->audio = audio;
 
     sc_vecdeque_init(&recorder->video_queue);
@@ -680,13 +699,15 @@ sc_recorder_init(struct sc_recorder *recorder, const char *filename,
     recorder->cbs = cbs;
     recorder->cbs_userdata = cbs_userdata;
 
-    static const struct sc_packet_sink_ops video_ops = {
-        .open = sc_recorder_video_packet_sink_open,
-        .close = sc_recorder_video_packet_sink_close,
-        .push = sc_recorder_video_packet_sink_push,
-    };
+    if (video) {
+        static const struct sc_packet_sink_ops video_ops = {
+            .open = sc_recorder_video_packet_sink_open,
+            .close = sc_recorder_video_packet_sink_close,
+            .push = sc_recorder_video_packet_sink_push,
+        };
 
-    recorder->video_packet_sink.ops = &video_ops;
+        recorder->video_packet_sink.ops = &video_ops;
+    }
 
     if (audio) {
         static const struct sc_packet_sink_ops audio_ops = {

hylcore-V/trading-bot

commit 332996721fbcb4a8629ca9a1add89200a3852c75

Fix adb forward initialization
In forward mode, the dummy byte must be written immediately after the
first accept(), otherwise the client will wait indefinitely, causing a
deadlock (or a timeout).

Regression introduced by 8c650e53cd37a53d5c3aa746c30a71c6b742a4e2.

server/src/main/java/com/genymobile/scrcpy/DesktopConnection.java
@@ -64,8 +64,6 @@ public final class DesktopConnection implements Closeable {
             throws IOException {
         String socketName = getSocketName(scid);
 
-        LocalSocket firstSocket = null;
-
         LocalSocket videoSocket = null;
         LocalSocket audioSocket = null;
         LocalSocket controlSocket = null;
@@ -74,23 +72,27 @@ public final class DesktopConnection implements Closeable {
                 try (LocalServerSocket localServerSocket = new LocalServerSocket(socketName)) {
                     if (video) {
                         videoSocket = localServerSocket.accept();
-                        firstSocket = videoSocket;
+                        if (sendDummyByte) {
+                            // send one byte so the client may read() to detect a connection error
+                            videoSocket.getOutputStream().write(0);
+                            sendDummyByte = false;
+                        }
                     }
                     if (audio) {
                         audioSocket = localServerSocket.accept();
-                        if (firstSocket == null) {
-                            firstSocket = audioSocket;
+                        if (sendDummyByte) {
+                            // send one byte so the client may read() to detect a connection error
+                            audioSocket.getOutputStream().write(0);
+                            sendDummyByte = false;
                         }
                     }
                     if (control) {
                         controlSocket = localServerSocket.accept();
-                        if (firstSocket == null) {
-                            firstSocket = controlSocket;
+                        if (sendDummyByte) {
+                            // send one byte so the client may read() to detect a connection error
+                            controlSocket.getOutputStream().write(0);
+                            sendDummyByte = false;
                         }
-                    }
-                    if (sendDummyByte) {
-                        // send one byte so the client may read() to detect a connection error
-                        firstSocket.getOutputStream().write(0);
                     }
                 }
             } else {